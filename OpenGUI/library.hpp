8455aa48b708d415f709a8d49d97927bd9f8ae6e
g00seDevr <45496881+g00seDevr@users.noreply.github.com>
2018-12-01 18:27:00 +0300

    Update library.hpp

--- OpenGUI/library.hpp
+++ OpenGUI/library.hpp
@@ -4,24 +4,41 @@
 #include <iostream>
 #include <GL/glut.h>
 
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void display(void)
+{
+    //I'll leave the triangle here for a while.
+    //I'll redo it later.
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.5,-0.5,0.0);
+        glVertex3f(0.0,0.5,0.0);
+        glVertex3f(0.5,-0.5,0.0);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
 class library
 {
 private:
     const char *name;
     int width, height;
+
 public:
-    
+
     void setWindowSize(int width, int height)
     {
         this->width = width;
         this->height = height;
     }
-    
-    void display()
-    {
-        
-    }
-    
+
     void init(int argc, char **argv)
     {
         glutInit(&argc, argv);
@@ -29,6 +46,10 @@ public:
         glutInitWindowPosition(100,100);
         glutInitWindowSize(width, height);
         glutCreateWindow(name);
+        glViewport(0, 0, width, height);
+        
+        glutDisplayFunc(display);
+        glutReshapeFunc(reshape);
 
         glutMainLoop();
     }
