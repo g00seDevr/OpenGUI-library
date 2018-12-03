6af74a017a41d2e980606126e46f49aaee17dd46
g00seDevr <45496881+g00seDevr@users.noreply.github.com>
2018-12-03 18:31:12 +0300

    Update library.hpp

--- OpenGUI/library.hpp
+++ OpenGUI/library.hpp
@@ -1,32 +1,24 @@
 #ifndef LIBRARY_HPP
 #define LIBRARY_HPP
 
-#include <iostream>
 #include <GL/glut.h>
-#include <GUI_ellements/button.hpp">
+#include <vector>
+#include "GUI_ellements/button.hpp"
+#include "GUI_ellements/ellements.h"
+
+void display(void);
 
 void reshape(int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-void display(void)
-{
-    //I'll leave the button(rectangle) here for a while.
-    //I'll redo it later.
-    
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    button(-0.5, -0.5, 0.5, 0.5);
-
-    glutSwapBuffers();
-}
-
 class library
 {
 private:
     const char *name;
-    int width, height;
+    static int width, height;
+    std::vector<button> ellement;
 
 public:
 
@@ -36,6 +28,9 @@ public:
         this->height = height;
     }
 
+    static int getWidth() { return width; }
+    static int getHeight() { return height; }
+
     void init(int argc, char **argv)
     {
         glutInit(&argc, argv);
@@ -44,15 +39,33 @@ public:
         glutInitWindowSize(width, height);
         glutCreateWindow(name);
         glViewport(0, 0, width, height);
-        
+            
         glutDisplayFunc(display);
         glutReshapeFunc(reshape);
-
+        
         glutMainLoop();
+    }
+
+    std::vector<button> getEllements()
+    {
+        return ellement;
     }
 
     library(char *name):name(name) { }
     ~library() { }
 };
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0f, library::getWidth(), library::getHeight(), 0.0f, 0.0f, 1.0f);
+
+    
+
+    glutSwapBuffers();
+}
 
 #endif
